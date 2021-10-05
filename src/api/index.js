2fbcdb6fa89802bbfdbeb597ad32ba6ca87ea796
Stingray-M <mmatt246@gmail.com>
2021-10-05 15:37:45 -0400

    removed unnecessary files

--- src/api/index.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-const url = "https://cse416-training.herokuapp.com/interests";
-
-export const fetchInterests = () => axios.get(url);
-export const addInterest = (newInterest) => axios.post(url, newInterest);
